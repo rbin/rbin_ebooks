f92a7ecc47e3747f7f13088f729b5045ba3d7981
rbin <me@rbin.co>
2014-03-27 03:47:21 +0000

    HUNDRED DOLLA BILLS

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main ebooks.app
   :dependencies [[org.clojure/clojure "1.5.1"]
-  							[com.novemberain/monger "1.7.0"]
-  							[cheshire "5.1.1"]
-  							[twitter-api "0.7.5"]])
+                [com.novemberain/monger "1.7.0"]
+                [cheshire "5.1.1"]
+                [twitter-api "0.7.5"]])
--- src/ebooks/app.clj
+++ src/ebooks/app.clj
@@ -1,16 +1,16 @@
 (ns ebooks.app
-	(:require [monger.collection :as mc]
-						[monger.core :as mg]))
+  (:require [monger.collection :as mc]
+            [monger.core :as mg]))
 
-	;; localhost, default port
-	(mg/connect!)
-	(mg/set-db! (mg/get-db "rbin_ebooks"))
+  ;; localhost, default port
+  (mg/connect!)
+  (mg/set-db! (mg/get-db "rbin_ebooks"))
 
-	(def twitter-timeline "https://api.twitter.com/1.1/statuses/user_timeline.json")
+  (def twitter-timeline "https://api.twitter.com/1.1/statuses/user_timeline.json")
 
-	(defn -main []
+  (defn -main []
 
-		(mc/insert "documents" {:first_name "John"  :last_name "Lennon"})
+    (mc/insert "documents" {:first_name "John"  :last_name "Lennon"})
 
-		(println (mc/find-maps "documents" { :first_name "John"} )))
-		;; (mc/remove "documents")
+    (println (mc/find-maps "documents" { :first_name "John"} )))
+    ;; (mc/remove "documents")
